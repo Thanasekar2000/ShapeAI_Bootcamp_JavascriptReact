6e134918d9b916943b5c9ecea4596714276acc24
Thanasekar2000 <85397062+Thanasekar2000@users.noreply.github.com>
2021-06-05 17:49:15 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 align="center">Money Heist Series</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Season 01 to 04</h1>
+      <p>
+        The series traces heists led by the Professor. The first heist is Royal
+        Mint of Spain. The robbers escape successfully from the Mint with €984
+        million. The second heist is Bank of Spain. Sierra finding the
+        Professor's hideout, holding him at gunpoint. We are waiting for season
+        05.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
